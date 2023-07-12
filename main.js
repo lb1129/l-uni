ecd7683c03cf34fcfbd14b328e3a99724e8cac7d
leiboT <superviho1@gmail.com>
2023-07-13 00:25:06 +0800

    chore: 无用代码清除

--- main.js
+++ main.js
@@ -1,30 +1,14 @@
 import App from './App.vue'
+import { createSSRApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import messages from './locale/index'
 
-let i18nConfig = {
+const i18nConfig = {
   locale: uni.getLocale(),
   messages
 }
-
-
-// #ifndef VUE3
-import Vue from 'vue'
-import VueI18n from 'vue-i18n'
-Vue.use(VueI18n)
-const i18n = new VueI18n(i18nConfig)
-Vue.config.productionTip = false
-App.mpType = 'app'
-const app = new Vue({
-    ...App,
-	i18n
-})
-app.$mount()
-// #endif
-
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-import { createI18n } from 'vue-i18n'
 const i18n = createI18n(i18nConfig)
+
 export function createApp() {
   const app = createSSRApp(App)
   app.use(i18n)
@@ -32,4 +16,3 @@ export function createApp() {
     app
   }
 }
-// #endif
