874bc387f00bb12dba54cc04c86312a0f57d29f4
leiboT <superviho1@gmail.com>
2023-07-13 23:47:54 +0800

    chore: app-icon-text组件

---
+++ components/app-icon-text/app-icon-text.vue
@@ -0,0 +1,48 @@
+<template>
+	<view class="app-icon-text" :style="customStyle" @click="clickHandler">
+		<uni-icons :type="iconType" :color="iconColor" :custom-prefix="customPrefix" :size="iconSize"></uni-icons>
+		<view class="app-icon-text-text">
+			<slot></slot>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "app-icon-text",
+		emits: ['click'],
+		props: {
+			iconType: String,
+			iconSize: {
+				type: Number,
+				default: 30
+			},
+			iconColor: String,
+			customPrefix: String,
+			text: String,
+			customStyle: Object
+		},
+		options: {
+			virtualHost: true
+		},
+		methods: {
+			clickHandler(e) {
+				this.$emit('click', e)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.app-icon-text {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+		&-text {
+			min-height: 38rpx;
+		}
+	}
+</style>
