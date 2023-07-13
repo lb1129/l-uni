c955bb7136df33b4a5f6dab9963232839dd236d9
leiboT <superviho1@gmail.com>
2023-07-13 23:48:56 +0800

    chore: app-authenticate-layout组件

---
+++ components/app-authenticate-layout/app-authenticate-layout.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="app-authenticate-layout-header">
+		<app-avatar src="/static/image/logo.png" width="75px" height="75px" :custom-style="{
+			margin: '45px 0 10px 0'
+		}"></app-avatar>
+	</view>
+	<view class="app-authenticate-layout-content">
+		<slot></slot>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "app-authenticate-layout",
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.app-authenticate-layout-header {
+		text-align: center;
+	}
+
+	.app-authenticate-layout-content {
+		padding: 0 30px;
+	}
+</style>
