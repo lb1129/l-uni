e6304fa543c3db4bbcc306d83ddc76877d9bc9bb
leiboT <superviho1@gmail.com>
2023-07-13 23:54:18 +0800

    chore: info页面

---
+++ pages/personal-center/info/info.vue
@@ -0,0 +1,36 @@
+<template>
+	<uni-list>
+		<uni-list-item showArrow>
+			<template v-slot:header>
+				<view class="info-avatart-text">
+					<text style="font-size: 14px;">头像</text>
+				</view>
+			</template>
+			<template v-slot:footer>
+				<app-avatar src="/static/image/user.png" width="40px" height="40px"></app-avatar>
+			</template>
+		</uni-list-item>
+		<uni-list-item title="昵称" showArrow right-text="viho">
+		</uni-list-item>
+	</uni-list>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.info-avatart-text {
+		display: flex;
+		align-items: center;
+	}
+</style>
