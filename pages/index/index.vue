e14d239e511f518c0057a627d14e2d5c26662bff
leiboT <superviho1@gmail.com>
2023-07-13 23:51:13 +0800

    chore: 首页调整

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,58 +1,44 @@
 <template>
-	<view>
-		<text>{{$t('hello')}}</text>
-		<uni-icons custom-prefix="customicons" type="youxi"></uni-icons>
-		<uni-icons type="color"></uni-icons>
-		<picker @change="localeChange" :value="index" range-key="label" :range="locales">
-			<button>{{$t('settingLocale')}}</button>
-		</picker>
-	</view>
+	<template v-for="menu in menuData" :key="menu.name">
+		<uni-section :title="menu.name" type="line" padding>
+			<uni-grid :column="4">
+				<uni-grid-item v-for="child in menu.children" :key="child.name">
+					<app-icon-text :icon-type="child.icon"
+						@click="clickHandler(child.path)">{{child.name}}</app-icon-text>
+				</uni-grid-item>
+			</uni-grid>
+		</uni-section>
+		<app-gap></app-gap>
+	</template>
 </template>
 
 <script>
+	import userMenuData from '@/mock/userMenuData.json'
 	export default {
 		data() {
 			return {
-				index: 0,
-				locales: [
-					{value: 'en', label: 'English'},
-					{value: 'zh-Hans', label: '简体中文'}
-				]
+				menuData: userMenuData.viho
 			}
 		},
 		onLoad() {
-			const systemInfo = uni.getSystemInfoSync()
-			this.isAndroid = systemInfo.platform.toLowerCase() === 'android'
-			const currentLocale = uni.getLocale();
-			this.index = this.locales.findIndex(item => item.value === currentLocale)
+
 		},
 		methods: {
-			localeChange(event) {
-				const index = event.detail.value
-				// 未改变不切换
-				if(index === this.index) return
-				const locale = this.locales[index].value
-				if (this.isAndroid) {
-					// 将重启应用
-					uni.showModal({
-						content: this.$t('languageChangeConfirm'),
-						success: (res) => {
-							if (res.confirm) {
-								uni.setLocale(locale);
-								this.index = index;
-							}
-						}
-					})
-				} else {
-					uni.setLocale(locale);
-					this.$i18n.locale = locale;
-					this.index = index;
-				}
+			clickHandler(url) {
+				uni.navigateTo({
+					url
+				})
 			}
+		},
+		// 页面级别的下拉刷新
+		onPullDownRefresh() {
+			setTimeout(() => {
+				uni.stopPullDownRefresh()
+			}, 500)
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
 
 </style>
