3ffa762034408f59296cdd71d8aea80fcc62da9c
leiboT <superviho1@gmail.com>
2023-07-13 23:54:47 +0800

    chore: about-use页面

---
+++ pages/personal-center/about-us/about-us.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<text>关于我们</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
