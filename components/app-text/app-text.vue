b86be1070b21296a5368b1b5cebaa3638f710932
leiboT <superviho1@gmail.com>
2023-07-13 23:48:06 +0800

    chore: app-text组件

---
+++ components/app-text/app-text.vue
@@ -0,0 +1,42 @@
+<template>
+	<text :style="{fontSize: fontSize, ...extraStyle}" @click="clickHandler">
+		<slot></slot>
+	</text>
+</template>
+
+<script>
+	export default {
+		name: "app-text",
+		emits: ['click'],
+		props: {
+			theme: {
+				type: String,
+				default: '#007aff',
+			},
+			fontSize: {
+				type: String,
+				default: '14px'
+			},
+			likeLink: Boolean
+		},
+		computed: {
+			extraStyle() {
+				let res = {}
+				if (this.likeLink) {
+					res.color = this.theme
+					res.cursor = 'pointer'
+				}
+				return res
+			}
+		},
+		methods: {
+			clickHandler(e) {
+				this.$emit('click', e)
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
