7227bed9a84f6f5589d6926cf06e814786c43254
leiboT <superviho1@gmail.com>
2023-07-13 23:53:52 +0800

    chore: service-agreement页面

---
+++ pages/personal-center/service-agreement/service-agreement.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<text>服务协议</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
