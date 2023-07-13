1137ef2b582aaa5362e9edb25fbf7a2202229af6
leiboT <superviho1@gmail.com>
2023-07-13 23:54:09 +0800

    chore: privacy-policy页面

---
+++ pages/personal-center/privacy-policy/privacy-policy.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<text>隐私政策</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
