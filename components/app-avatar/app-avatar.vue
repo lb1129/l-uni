f0d5e12b2bf1b836b1701be9aaf6b327e48dd4a0
leiboT <superviho1@gmail.com>
2023-07-13 23:47:10 +0800

    chore: app-avatar

---
+++ components/app-avatar/app-avatar.vue
@@ -0,0 +1,32 @@
+<template>
+	<image class="app-avatar" mode="scaleToFill" :src="src" :style="{
+		width: width,
+		height: height,
+		...customStyle
+	}"></image>
+</template>
+
+<script>
+	export default {
+		name: "app-avatar",
+		props: {
+			// TODO 支持图标 形状
+			src: String,
+			width: {
+				type: String,
+				default: '40px'
+			},
+			height: {
+				type: String,
+				default: '40px'
+			},
+			customStyle: Object
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.app-avatar {
+		border-radius: 50%;
+	}
+</style>
