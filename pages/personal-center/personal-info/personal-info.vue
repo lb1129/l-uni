f0f854be85241f9201fcc36deeae7befcae20678
leiboT <superviho1@gmail.com>
2023-07-14 23:54:58 +0800

    chore: 改名&图片选择

---
+++ pages/personal-center/personal-info/personal-info.vue
@@ -0,0 +1,54 @@
+<template>
+	<uni-list>
+		<uni-list-item showArrow clickable @click="changeAvatarHandler">
+			<template v-slot:header>
+				<view class="info-avatart-text">
+					<text style="font-size: 14px;">{{$t('avatar')}}</text>
+				</view>
+			</template>
+			<template v-slot:footer>
+				<app-avatar :src="avatarUrl" width="40px" height="40px"></app-avatar>
+			</template>
+		</uni-list-item>
+		<uni-list-item :title="$t('nickname')" to="/pages/personal-center/edit-nickname/edit-nickname" showArrow right-text="viho">
+		</uni-list-item>
+	</uni-list>
+</template>
+
+<script>
+	import permision from "@/common/permission.js"
+	export default {
+		data() {
+			return {
+				avatarUrl: '/static/image/user.png'
+			}
+		},
+		methods: {
+			async changeAvatarHandler() {
+				uni.chooseImage({
+					count: 1,
+					sourceType: ['camera', 'album'],
+					sizeType: ['compressed', 'original'],
+					success: (res) => {
+						// TODO call upload api
+						// uni.uploadFile({
+						// 	url: '',
+						// 	filePath: res.tempFilePaths[0],
+						// 	name: 'file',
+						// 	success: (uploadFileRes) => {
+						// 		// this.avatarUrl = uploadFileRes.data.data
+						// 	}
+						// })
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.info-avatart-text {
+		display: flex;
+		align-items: center;
+	}
+</style>
