8dc95805441d9edb8b771feb08cc03cbb1982d04
leiboT <superviho1@gmail.com>
2023-07-14 23:45:43 +0800

    chore: store

---
+++ store/index.js
@@ -0,0 +1,49 @@
+import {
+	createStore
+} from 'vuex'
+
+const store = createStore({
+	state() {
+		return {
+			theme: '#007aff',
+			userInfo: {},
+			menuData: []
+		}
+	},
+	mutations: {
+		updateTheme(state, theme) {
+			state.theme = theme
+		},
+		updateUserInfo(state, userInfo) {
+			state.userInfo = userInfo
+		},
+		updateMenuData(state, menuData) {
+			state.menuData = menuData
+		}
+	},
+	getters: {
+		nickname(state) {
+			return state.userInfo.nickname
+		}
+	},
+	actions: {
+		setTheme({
+			commit
+		}, theme) {
+			commit('updateTheme', theme)
+		},
+		setUserInfo({
+			commit
+		}, userInfo) {
+			commit('updateUserInfo', userInfo)
+		},
+		setMenuData({
+			commit
+		}, menuData) {
+			commit('updateMenuData', menuData)
+		}
+	},
+	modules: {}
+})
+
+export default store
