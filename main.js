8acca00f3af285a08f10cc611eea663de800748e
leiboT <superviho1@gmail.com>
2023-07-15 00:04:05 +0800

    chore: mixin $theme & 处理导航栏背景标题及tabbar样式

--- main.js
+++ main.js
@@ -1,18 +1,80 @@
 import App from './App.vue'
-import { createSSRApp } from 'vue'
-import { createI18n } from 'vue-i18n'
+import {
+	createSSRApp
+} from 'vue'
+import {
+	createI18n
+} from 'vue-i18n'
+import store from '@/store/index'
 import messages from './locale/index'
+// #ifdef MP-WEIXIN
+import {
+	camelCase
+}
+from 'lodash-es'
+// #endif
 
 const i18nConfig = {
-  locale: uni.getLocale(),
-  messages
+	locale: uni.getLocale(),
+	messages
 }
 const i18n = createI18n(i18nConfig)
 
 export function createApp() {
-  const app = createSSRApp(App)
-  app.use(i18n)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	app.use(i18n)
+	app.use(store)
+	app.mixin({
+		computed: {
+			$theme() {
+				return this.$store.state.theme
+			}
+		},
+		onShow() {
+			setTimeout(() => {
+				const pages = getCurrentPages();
+				const page = pages[pages.length - 1];
+				// 有页面时
+				if (page) {
+					const themeVal = this.$theme.slice(1)
+					// 设置导航栏背景色
+					uni.setNavigationBarColor({
+						frontColor: '#ffffff',
+						backgroundColor: this.$theme
+					})
+					// tabBar页面时
+					if (page.route === 'pages/home/home' || page.route === 'pages/me/me') {
+						// 设置tabBar
+						uni.setTabBarItem({
+							index: 0,
+							// #ifdef MP-WEIXIN
+							text: this.$t('home'),
+							// #endif
+							selectedIconPath: `/static/tabbar/home-${themeVal}.png`,
+						})
+						uni.setTabBarItem({
+							index: 1,
+							// #ifdef MP-WEIXIN
+							text: this.$t('me'),
+							// #endif
+							selectedIconPath: `/static/tabbar/user-${themeVal}.png`,
+						})
+						uni.setTabBarStyle({
+							selectedColor: this.$theme
+						})
+					}
+					// #ifdef MP-WEIXIN
+					const arr = page.route.split('/')
+					const str = arr[arr.length - 1]
+					uni.setNavigationBarTitle({
+						title: this.$t(camelCase(str))
+					})
+					// #endif
+				}
+			})
+		}
+	})
+	return {
+		app
+	}
 }
