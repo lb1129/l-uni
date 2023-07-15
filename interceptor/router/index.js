1799abf0ca8c9ceb5da0d8b8936f6aeb0dc7b7d6
leiboT <superviho1@gmail.com>
2023-07-15 16:59:36 +0800

    chore: 路由 拦截器

---
+++ interceptor/router/index.js
@@ -0,0 +1,58 @@
+import isAuthenticated from '@/interceptor/router/isAuthenticated.js'
+import {
+	authenticatePageUrls,
+	specialPageUrls
+} from './pages.js'
+import {
+	pathFull
+} from '@/common/pathFull.js'
+
+// 全端路由跳转方法拦截器 处理应用内跳转
+
+const validate = async (args) => {
+	const url = pathFull(args.url)
+	try {
+		await isAuthenticated.value
+		if (authenticatePageUrls.includes(url)) {
+			uni.switchTab({
+				url: '/pages/home/home'
+			})
+			return Promise.reject(args)
+		} else {
+			return args
+		}
+	} catch (e) {
+		if (!authenticatePageUrls.includes(url) && !specialPageUrls.includes(url)) {
+			uni.reLaunch({
+				url: '/pages/login/login'
+			})
+			return Promise.reject(args)
+		} else {
+			return args
+		}
+	}
+}
+
+uni.addInterceptor('navigateTo', {
+	invoke(args) {
+		return validate(args)
+	}
+})
+
+uni.addInterceptor('redirectTo', {
+	invoke(args) {
+		return validate(args)
+	}
+})
+
+uni.addInterceptor('reLaunch', {
+	invoke(args) {
+		return validate(args)
+	}
+})
+
+uni.addInterceptor('switchTab', {
+	invoke(args) {
+		return validate(args)
+	}
+})
---
+++ interceptor/router/pages.js
@@ -0,0 +1,11 @@
+// （已登录需自动跳转首页）登录 注册 找回密码 ...
+export const authenticatePageUrls = [
+	'/pages/login/login',
+	'/pages/authenticate/register/register',
+	'/pages/authenticate/find-password/find-password',
+]
+// (无论是否登录 都能访问) 隐私政策 启动页 ...
+export const specialPageUrls = [
+	'/pages/personal-center/privacy-policy/privacy-policy',
+	'/pages/start/start',
+]
