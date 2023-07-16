7912694159395c2f45b86ca482fc1b01330f7e48
leiboT <superviho1@gmail.com>
2023-07-17 01:24:20 +0800

    refactor: 重命名&完成页面逻辑

---
+++ pages/product-manage/product-detail/product-detail.vue
@@ -0,0 +1,112 @@
+<template>
+	<swiper class="detail-swiper" circular indicator-dots :indicator-color="$theme">
+		<swiper-item>
+			<view class="detail-swiper-item">
+				<image mode="aspectFill" class="detail-swiper-item-image" src="https://img14.360buyimg.com/n1/s450x450_jfs/t1/141480/8/14039/48663/5fab0686E7d32b85f/bb2489ac5d2b4ed5.jpg"></image>
+			</view>
+		</swiper-item>
+		<swiper-item>
+			<view class="detail-swiper-item">
+				<image mode="aspectFill" class="detail-swiper-item-image" src="https://img13.360buyimg.com/n1/s450x450_jfs/t1/199961/6/36523/18216/647ef573F5cd51081/05b06c8d982c2162.jpg"></image>
+			</view>
+		</swiper-item>
+	</swiper>
+	<view class="detail-content">
+		<view class="detail-content-item">
+			<text class="detail-content-item-label">名称</text>
+			<text class="detail-content-item-value">{{record.name}}</text>
+		</view>
+		<view class="detail-content-item">
+			<text class="detail-content-item-label">品牌</text>
+			<text class="detail-content-item-value">{{record.brand}}</text>
+		</view>
+		<view class="detail-content-item">
+			<text class="detail-content-item-label">类别</text>
+			<text class="detail-content-item-value">{{record.category}}</text>
+		</view>
+		<view class="detail-content-item">
+			<text class="detail-content-item-label">价格</text>
+			<text class="detail-content-item-value">{{record.price}}</text>
+		</view>
+		<view class="detail-content-item">
+			<text class="detail-content-item-label">颜色</text>
+			<text class="detail-content-item-value">{{record.color}}</text>
+		</view>
+		<view class="detail-content-item">
+			<text class="detail-content-item-label">款式</text>
+			<text class="detail-content-item-value">{{record.style}}</text>
+		</view>
+		<view class="detail-content-item">
+			<text class="detail-content-item-label">库存</text>
+			<text class="detail-content-item-value">{{record.inventory}}</text>
+		</view>
+		<view class="detail-content-item">
+			<text class="detail-content-item-label">描述</text>
+			<text class="detail-content-item-value">{{record.describe}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	import {
+		getProductByIdServe
+	} from '@/serves/product.js'
+	export default {
+		data() {
+			return {
+				record: {}
+			}
+		},
+		inheritAttrs: false,
+		async onLoad(e) {
+			const id = e.id
+			if (id) {
+				try {
+					const res = await getProductByIdServe(id)
+					this.record = res.data
+				} catch (e) {}
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.detail-swiper {
+		height: 200px;
+
+		&-item {
+			display: block;
+			height: 200px;
+			line-height: 200px;
+			text-align: center;
+
+			&-image {
+				width: 100%;
+				height: 100%;
+			}
+		}
+	}
+
+	.detail-content {
+		padding: 8px 16px;
+
+		&-item {
+			padding: 8px 0;
+
+			&-label {
+				font-size: 16px;
+				font-weight: 700;
+
+				&::after {
+					content: ':';
+					margin-left: 2px;
+					margin-right: 4px;
+				}
+			}
+
+			&-value {
+				font-size: 14px;
+			}
+		}
+	}
+</style>
---
+++ pages/product-manage/product-list/product-list.vue
@@ -0,0 +1,169 @@
+<template>
+	<uni-search-bar :placeholder="$t('searchByName')" :cancelText="$t('cancel')" @confirm="searchHandler"
+		@clear="searchClearHandler" @cancel="searchClearHandler" clearButton="none">
+	</uni-search-bar>
+	<uni-list>
+		<uni-swipe-action>
+			<uni-swipe-action-item v-for="(item, index) in dataSource" :key="item.id" :right-options="options"
+				@click="actionItemClickHandler($event, item, index)">
+				<uni-list-chat clickable @click="itemClickHandler(item)" :avatar-circle="true" :title="item.name"
+					avatar="/static/image/logo.png" :note="item.brand" :time="`￥${item.price}`"></uni-list-chat>
+			</uni-swipe-action-item>
+		</uni-swipe-action>
+	</uni-list>
+	<uni-load-more id="loadMore" :color="$theme" :status="status" />
+</template>
+
+<script>
+	import {
+		getProductsServe,
+		deleteProductByIdsServe
+	} from '@/serves/product.js'
+	import qs from 'query-string'
+	import {
+		operateAuthMixin
+	} from '@/interceptor/router/auth.js'
+	export default {
+		// 操作权限
+		mixins: [operateAuthMixin],
+		data() {
+			return {
+				loading: false,
+				queryParams: {
+					keyword: '',
+					pagination: {
+						pageNo: 1,
+						pageSize: 10
+					},
+				},
+				dataSource: [],
+				total: 0
+			}
+		},
+		computed: {
+			options() {
+				return [{
+					text: this.$t('delete'),
+					style: {
+						color: this.$canDelete ? '#ffffff' : 'rgb(0 0 0 / 30%)',
+						backgroundColor: this.$canDelete ? this.$theme : '#f7f7f7'
+					}
+				}]
+			},
+			status() {
+				if (this.loading) return 'loading'
+				if (this.total <= this.dataSource.length) return 'no-more'
+				return 'more'
+			}
+		},
+		methods: {
+			actionItemClickHandler(e, record, index) {
+				// 删除
+				if (e.index === 0) {
+					if (this.$canDelete) {
+						uni.showModal({
+							title: this.$t('tip'),
+							content: this.$t('areYouSureDelete'),
+							cancelText: this.$t('cancel'),
+							confirmText: this.$t('confirm'),
+							success: async (res) => {
+								if (res.confirm) {
+									try {
+										await deleteProductByIdsServe([record.id])
+										this.dataSource.splice(index, 1)
+											--this.total
+										if (this.status === 'more') {
+											const query = uni.createSelectorQuery().in(this);
+											query.select('#loadMore').boundingClientRect(({
+												top
+											}) => {
+												const {
+													windowHeight,
+													windowTop
+												} = uni.getWindowInfo()
+												if (top - windowHeight - windowTop <= 50) {
+													this.queryParams.pagination.pageNo++
+													this.loadData()
+												}
+											}).exec();
+										}
+
+									} catch (e) {}
+								}
+							}
+						})
+					}
+				}
+			},
+			itemClickHandler(record) {
+				if (this.$canDetail) {
+					uni.navigateTo({
+						url: qs.stringifyUrl({
+							url: '/pages/product-manage/product-detail/product-detail',
+							query: {
+								id: record.id
+							}
+						})
+					})
+				}
+
+			},
+			searchClearHandler() {
+				if (this.queryParams.keyword) {
+					this.queryParams.keyword = ''
+					this.queryParams.pagination.pageNo = 1
+					this.dataSource = []
+					this.loadData()
+				}
+			},
+			searchHandler(e) {
+				const value = e.value
+				if (this.queryParams.keyword !== value) {
+					this.queryParams.keyword = value
+					this.queryParams.pagination.pageNo = 1
+					this.dataSource = []
+					this.loadData()
+				}
+			},
+			async loadData() {
+				this.loading = true
+				try {
+					const res = await getProductsServe(this.queryParams)
+					this.dataSource = res.data.data
+					this.total = res.data.total
+					this.loading = false
+				} catch (error) {
+					this.loading = false
+				}
+			}
+		},
+		onPullDownRefresh() {
+			this.queryParams.pagination.pageNo = 1
+			this.dataSource = []
+			this.loadData().finally(() => {
+				uni.stopPullDownRefresh()
+			})
+		},
+		onReachBottom() {
+			if (this.status === 'more') {
+				this.queryParams.pagination.pageNo++
+				this.loadData()
+			}
+		},
+		onReady() {
+			// 根据屏幕高度 计算一页需要多少条才能触发加载更多
+			const {
+				windowHeight
+			} = uni.getWindowInfo()
+			const searchBarHeight = 56
+			const listItemHeight = 67
+			const pageSize = Math.ceil((windowHeight - searchBarHeight) / listItemHeight) + 1
+			this.queryParams.pagination.pageSize = pageSize
+			this.loadData()
+		}
+	}
+</script>
+
+<style>
+
+</style>
