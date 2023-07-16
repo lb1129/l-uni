29a45ca082f0c3371d3ea823850f7f521d7b4238
leiboT <superviho1@gmail.com>
2023-07-17 01:28:02 +0800

    chore: h5 404页面

---
+++ pages/h5-404/h5-404.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="page-not-found">
+		<text class="page-not-found-text">{{$t('pageNotFound')}}</text>
+		<app-gap></app-gap>
+		<app-button @click="goBackHandler">{{$t('back')}}</app-button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+			goBackHandler() {
+				this.$router.back()
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.page-not-found {
+		padding: 150px 16px 0;
+		text-align: center;
+
+		&-text {
+			font-size: 16px;
+		}
+	}
+</style>
