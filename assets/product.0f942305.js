ffb406788b3ffcc130ea919d5283f66e9e6833da
leiboT <superviho1@gmail.com>
2023-07-17 22:45:21 +0800

    deploy

---
+++ assets/product.0f942305.js
@@ -0,0 +1,1 @@
+import{ax as a,as as s,ay as r,az as t,aA as d,aB as o}from"./index-6f0a2feb.js";const e=t=>a({url:s.stringifyUrl({url:r,query:{id:t}})}),i=a=>t({url:r,method:"DELETE",data:a}),l=a=>d({url:o,method:"POST",data:a});export{e as a,i as d,l as g};
