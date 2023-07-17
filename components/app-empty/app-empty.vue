c00c67cdce9ba87ab662dd9636fa8cd05e6b882d
leiboT <superviho1@gmail.com>
2023-07-17 19:27:23 +0800

    chore: app-empty组件

---
+++ components/app-empty/app-empty.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="app-empty">
+		<uni-icons custom-prefix="iconfont" type="icon-empty" size="40" color="#dddddd"></uni-icons>
+		<view class="app-empty-text">
+			<text>{{this.$t('noData')}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "app-empty"
+	}
+</script>
+
+<style lang="scss">
+	.app-empty {
+		text-align: center;
+
+		&-text {
+			font-size: 14px;
+			color: #dddddd;
+			margin-top: 16px;
+		}
+	}
+</style>
