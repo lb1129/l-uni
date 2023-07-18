5d46da300d56cee7f74a3482e727b35439cc2ceb
leiboT <superviho1@gmail.com>
2023-07-18 21:49:47 +0800

    chore: 初始wgt热更新&已登录跳首页

--- App.vue
+++ App.vue
@@ -16,24 +16,34 @@
 		getMenuServe,
 		getUserInfoServe
 	} from '@/serves/user.js'
+	// #ifdef APP-PLUS
+	import wgtUpdate from '@/common/wgtUpdate.js'
+	// #endif
 	export default {
 		onLaunch() {
+			// #ifdef APP-PLUS  
+			wgtUpdate()
+			// #endif
 			// 初始主题色
 			const theme = themeStorage.get()
 			if (theme) {
 				this.setTheme(theme)
 			}
 			// 如果已登录 获取用户信息 菜单数据 并更新 store
-			isLoginServe().then(async () => {
-				try {
-					getUserInfoServe().then(res => {
-						this.setUserInfo(res.data)
+			isLoginServe().then(() => {
+				const pages = getCurrentPages()
+				const page = pages[pages.length - 1]
+				if (page && page.route !== 'pages/home/home')
+					uni.switchTab({
+						url: '/pages/home/home'
 					})
-					getMenuServe().then(res => {
-						this.setMenuData(res.data)
-					})
-				} catch (e) {}
-			})
+				getUserInfoServe().then(res => {
+					this.setUserInfo(res.data)
+				})
+				getMenuServe().then(res => {
+					this.setMenuData(res.data)
+				})
+			}).catch(e => {})
 		},
 		onShow() {},
 		onHide() {},
