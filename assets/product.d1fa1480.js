571a1779d07ff5996ce9b1fc4ffa63a32daf454d
leiboT <superviho1@gmail.com>
2023-07-18 22:11:57 +0800

    deploy

---
+++ assets/product.d1fa1480.js
@@ -0,0 +1,1 @@
+import{aw as a,ar as s,ax as r,ay as t,az as d,aA as o}from"./index-f4a39db8.js";const e=t=>a({url:s.stringifyUrl({url:r,query:{id:t}})}),i=a=>t({url:r,method:"DELETE",data:a}),l=a=>d({url:o,method:"POST",data:a});export{e as a,i as d,l as g};
---
+++ assets/uni-app.es.6368d139.js
@@ -0,0 +1,1 @@
+import{as as r}from"./index-f4a39db8.js";function n(n,o){return r(n)?o:n}export{n as r};
