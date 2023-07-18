d1de419424718aa69b4c13beabbe1cd0efb04dbb
leiboT <superviho1@gmail.com>
2023-07-18 21:48:52 +0800

    chore: 完善request拦截器

--- interceptor/request/index.js
+++ interceptor/request/index.js
@@ -1,8 +1,10 @@
 import {
 	tokenStorage
 } from '@/storage/index.js'
-import isAuthenticated from '@/auth/isAuthenticated.js'
 import config from '@/config.json'
+import {
+	isFunction
+} from 'lodash-es'
 
 const baseUrl = config.baseUrl
 const timeout = config.timeout
@@ -20,34 +22,66 @@ uni.addInterceptor('request', {
 		}
 		if (!args.timeout) args.timeout = timeout
 	},
-	async returnValue(p) {
-		const res = await p
-		// 成功 返回 data
-		if (res.statusCode >= 200 && res.statusCode < 300) {
-			// 开发环境 log data
-			if (process.env.NODE_ENV === 'development')
-				console.log(res.data)
-			return res.data
-		} else {
+	success(res, req) {
+		// 服务器只要响应 不管http状态码多少 都会进来
+		if (process.env.NODE_ENV === 'development') {
+			console.log(req.url)
+			console.log(res.data)
+		}
+		// 使用 callback 形式
+		const useCallback = req.success || req.fail || req.complete
+		if (useCallback) {
 			// 401 跳转登录页
 			if (res.statusCode === 401) {
 				const pages = getCurrentPages()
 				const page = pages[pages.length - 1]
-				if (page.route !== 'pages/login/login') {
-					isAuthenticated.value = Promise.reject('isAuthenticated false')
+				if (page && page.route !== 'pages/login/login') {
 					tokenStorage.clear()
 					uni.reLaunch({
 						url: '/pages/login/login'
 					})
 				}
-			} else {
-				// 其他状态码 进行提示
-				uni.showToast({
-					icon: 'none',
-					title: `${res.data.message}`
-				})
 			}
-			throw res
+		}
+		if (!req.hiddenErrMsg && res.statusCode >= 300)
+			uni.showToast({
+				icon: 'none',
+				title: res.data.message
+			})
+	},
+	fail(e) {
+		// 服务器未响应错误 服务器不存在 timeout 服务器宕机
+		if (process.env.NODE_ENV === 'development')
+			uni.showToast({
+				icon: 'none',
+				title: e.errMsg
+			})
+	},
+	async returnValue(p) {
+		// 推荐外部使用 promise | async await 处理异步
+		// 如果外部使用 callback 则由外部处理 http状态
+		if (isFunction(p.then)) {
+			const res = await p
+			// 成功 返回 data
+			if (res.statusCode >= 200 && res.statusCode < 300) {
+				return res.data
+			} else {
+				// 401 跳转登录页
+				if (res.statusCode === 401) {
+					const pages = getCurrentPages()
+					const page = pages[pages.length - 1]
+					if (page && page.route !== 'pages/login/login') {
+						isAuthenticated.value = Promise.reject('isAuthenticated false')
+						tokenStorage.clear()
+						uni.reLaunch({
+							url: '/pages/login/login'
+						})
+					}
+				}
+				throw res
+			}
+		} else {
+			return p
 		}
 	}
 })
