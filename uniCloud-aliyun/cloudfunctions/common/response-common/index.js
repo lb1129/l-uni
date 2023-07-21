950aad0fe74c53890aac17639d7508b3bed3bbc4
leiboT <superviho1@gmail.com>
2023-07-21 11:46:42 +0800

    feat: 切分成功或失败的响应

--- uniCloud-aliyun/cloudfunctions/common/response-common/index.js
+++ uniCloud-aliyun/cloudfunctions/common/response-common/index.js
@@ -1,8 +1,18 @@
-module.exports = function(data = null, errCode = 0, errMsg = 'success') {
-	// 公用模块用法请参考 https://uniapp.dcloud.io/uniCloud/cf-common
-	return {
-		errCode,
-		errMsg,
-		data
+// 公用模块用法请参考 https://uniapp.dcloud.io/uniCloud/cf-common
+module.exports = {
+	success(data = null, errCode = 0, errMsg = 'success') {
+
+		return {
+			errCode,
+			errMsg,
+			data
+		}
+	},
+	fail(errMsg = 'fail', errCode = 1) {
+		return {
+			errCode,
+			errMsg,
+			data: null
+		}
 	}
 }
