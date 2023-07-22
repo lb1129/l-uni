02ad8318db48f06606b7060af1ede6bb895af24a
leiboT <superviho1@gmail.com>
2023-07-22 18:10:04 +0800

    chore: sendCode云函数

--- uniCloud-aliyun/cloudfunctions/sendCode/index.js
+++ uniCloud-aliyun/cloudfunctions/sendCode/index.js
@@ -3,6 +3,10 @@ const {
 	success,
 	fail
 } = require('response-common')
+const {
+	validatePhone
+} = require('validate-common')
+
 /**
  * 发送手机短信验证码
  * @param {object} event {phone: number}
@@ -12,7 +16,12 @@ exports.main = async (event = {}, context) => {
 	const {
 		phone
 	} = event
-	// TODO 调用三方短信服务
-
-	return success(1234)
+	// TODO 开通短信服务 新增uni-cloud-sms扩展 调用 uniCloud.sendSms
+	if (!validatePhone(phone))
+		return fail('手机号格式不正确')
+	const code = Math.floor(Math.random() * 89999 + 10000)
+	// 以手机号为key加入redis
+	// 60s过期
+	const res = await uniCloud.redis().set(phone, code, 'EX', 60)
+	return success(code)
 };
