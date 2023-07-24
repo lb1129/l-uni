2d77e82a72f515e1232f98116cea8ec3b470f2e3
leiboT <superviho1@gmail.com>
2023-07-24 11:15:55 +0800

    feat: 新增isNoValue校验&已有方法命名规范

--- uniCloud-aliyun/cloudfunctions/common/validate-common/index.js
+++ uniCloud-aliyun/cloudfunctions/common/validate-common/index.js
@@ -1,8 +1,14 @@
 module.exports = {
-	validatePhone(phone) {
+	isPhone(phone) {
 		return /^1[3456789]\d{9}$/.test(phone)
 	},
-	validatePassword(password) {
+	isPassword(password) {
 		return /^[a-zA-Z0-9]+$/.test(password)
+	},
+	isNoValue(value) {
+		if (value === null) return true
+		if (value === undefined) return true
+		if (typeof value === 'string' && !value) return true
+		return false
 	}
 }
