c3ee8fc0638d3b88a778b8503adca78ac4d638d8
leiboT <superviho1@gmail.com>
2023-07-24 11:17:30 +0800

    refactor: 参数校验调整

--- uniCloud-aliyun/cloudfunctions/auth/index.obj.js
+++ uniCloud-aliyun/cloudfunctions/auth/index.obj.js
@@ -5,20 +5,17 @@ const {
 	fail
 } = require('response-common')
 const {
-	validatePhone,
-	validatePassword
+	isPhone,
+	isPassword,
+	isNoValue
 } = require('validate-common')
 const {
 	sign
 } = require('jwt-common')
-// const uniID = require('uni-id-common')
+
 module.exports = {
 	_before: function() {
-		// FIXME uni-id 初始化报错
-		// const clientInfo = this.getClientInfo()
-		// this.uniID = uniID.createInstance({ // 创建uni-id实例，其上方法同uniID
-		// 	clientInfo
-		// })
+
 	},
 	/**
 	 * 登录
@@ -30,6 +27,10 @@ module.exports = {
 			username,
 			password
 		} = ops
+
+		if (isNoValue(username)) return fail('缺失用户名')
+		if (isNoValue(password)) return fail('缺失密码')
+
 		try {
 			const db = uniCloud.database()
 			const dbCmd = db.command
@@ -64,14 +65,16 @@ module.exports = {
 			phone,
 			code
 		} = ops
-		if (!username) {
-			return fail('缺失用户名')
-		}
-		if (!password) {
-			return fail('缺失密码')
-		}
-		const aes = uniCloud.importObject('aes')
+
+		if (isNoValue(username)) return fail('缺失用户名')
+		if (isNoValue(password)) return fail('缺失密码')
+		if (password !== confirmPassword) return fail('两次密码输入不一致')
+		if (isNoValue(phone)) return fail('缺失手机号')
+		if (!isPhone(phone)) return fail('手机号格式不正确')
+		if (isNoValue(code)) return fail('缺失验证码')
+
 		try {
+			const aes = uniCloud.importObject('aes')
 			const decryptRes = await aes.decrypt({
 				data: password
 			})
@@ -81,32 +84,18 @@ module.exports = {
 			if (decryptRes.data.length < 7 || decryptRes.data.length > 10) {
 				return fail('密码长度应控制在7-10之间')
 			}
-			if (!validatePassword(decryptRes.data)) {
+			if (!isPassword(decryptRes.data)) {
 				return fail('密码应为数字，英文大小写的组合')
 			}
 		} catch (e) {
 			return fail('密码未正确加密')
 		}
-		if (password !== confirmPassword) {
-			return fail('两次密码输入不一致')
-		}
-		if (!phone) {
-			return fail('缺失手机号')
-		}
-		if (!validatePhone(phone)) {
-			return fail('手机号格式不正确')
-		}
-		if (!code) {
-			return fail('缺失验证码')
-		}
-		const hadCode = await uniCloud.redis().get(phone)
-		if (!hadCode) {
-			return fail('请先获取验证码')
-		}
-		if (hadCode !== String(code)) {
-			return fail('验证码不匹配')
-		}
+
 		try {
+			const hadCode = await uniCloud.redis().get(phone)
+			if (isNoValue(hadCode)) return fail('请先获取验证码')
+			if (hadCode !== String(code)) return fail('验证码不匹配')
+
 			const db = uniCloud.database()
 			const collection = db.collection('user')
 			const hadUsernameRes = await collection.where({
@@ -143,11 +132,15 @@ module.exports = {
 			phone,
 			code
 		} = ops
-		if (!password) {
-			return fail('缺失密码')
-		}
-		const aes = uniCloud.importObject('aes')
+
+		if (isNoValue(password)) return fail('缺失密码')
+		if (password !== confirmPassword) return fail('两次密码输入不一致')
+		if (isNoValue(phone)) return fail('缺失手机号')
+		if (!isPhone(phone)) return fail('手机号格式不正确')
+		if (isNoValue(code)) return fail('缺失验证码')
+
 		try {
+			const aes = uniCloud.importObject('aes')
 			const decryptRes = await aes.decrypt({
 				data: password
 			})
@@ -157,32 +150,18 @@ module.exports = {
 			if (decryptRes.data.length < 7 || decryptRes.data.length > 10) {
 				return fail('密码长度应控制在7-10之间')
 			}
-			if (!validatePassword(decryptRes.data)) {
+			if (!isPassword(decryptRes.data)) {
 				return fail('密码应为数字，英文大小写的组合')
 			}
 		} catch (e) {
 			return fail('密码未正确加密')
 		}
-		if (password !== confirmPassword) {
-			return fail('两次密码输入不一致')
-		}
-		if (!phone) {
-			return fail('缺失手机号')
-		}
-		if (!validatePhone(phone)) {
-			return fail('手机号格式不正确')
-		}
-		if (!code) {
-			return fail('缺失验证码')
-		}
-		const hadCode = await uniCloud.redis().get(phone)
-		if (!hadCode) {
-			return fail('请先获取验证码')
-		}
-		if (hadCode !== String(code)) {
-			return fail('验证码不匹配')
-		}
+
 		try {
+			const hadCode = await uniCloud.redis().get(phone)
+			if (isNoValue(hadCode)) return fail('请先获取验证码')
+			if (hadCode !== String(code)) return fail('验证码不匹配')
+
 			const db = uniCloud.database()
 			const collection = db.collection('user')
 			const res = await collection.where({
@@ -204,6 +183,9 @@ module.exports = {
 		const {
 			id
 		} = ops
+
+		if (isNoValue(id)) return fail('缺失id')
+
 		try {
 			const db = uniCloud.database()
 			const res = await db.collection('user').doc(id).field({
